574daef56d5edbbe30748f82f5694718205f63e1
Ahmed Khitaby <ahmedkhitaby@gmail.com>
2022-06-28 16:51:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,268 @@
+const data = [
+  {
+    invoiced_amount_in_limit_currency: 594909.02,
+    actual_payment_date: "2022-01-12 00:00:00+01:00",
+    actual_settlement_date: null,
+    date: "2022-01-12 00:00:00+01:00",
+    status: "paid",
+  },
+  {
+    invoiced_amount_in_limit_currency: 265240.85,
+    actual_payment_date: "2020-11-03 00:00:00+01:00",
+    actual_settlement_date: "2021-03-03 15:50:31+01:00",
+    date: "2020-11-03 00:00:00+01:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 545482.82,
+    actual_payment_date: "2021-07-06 00:00:00+02:00",
+    actual_settlement_date: "2021-11-29 00:00:00+01:00",
+    date: "2021-07-06 00:00:00+02:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 251239.97,
+    actual_payment_date: "2021-05-11 00:00:00+02:00",
+    actual_settlement_date: "2021-09-10 00:00:00+02:00",
+    date: "2021-05-11 00:00:00+02:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 521500.61,
+    actual_payment_date: "2020-08-07 00:00:00+02:00",
+    actual_settlement_date: "2020-11-03 14:02:45+01:00",
+    date: "2020-08-07 00:00:00+02:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 808936.42,
+    actual_payment_date: "2022-03-25 00:00:00+01:00",
+    actual_settlement_date: null,
+    date: "2022-03-25 00:00:00+01:00",
+    status: "paid",
+  },
+  {
+    invoiced_amount_in_limit_currency: 801553.23,
+    actual_payment_date: "2022-04-01 00:00:00+02:00",
+    actual_settlement_date: null,
+    date: "2022-04-01 00:00:00+02:00",
+    status: "paid",
+  },
+  {
+    invoiced_amount_in_limit_currency: 356696.1,
+    actual_payment_date: "2021-06-23 00:00:00+02:00",
+    actual_settlement_date: "2021-10-21 00:00:00+02:00",
+    date: "2021-06-23 00:00:00+02:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 519419.4,
+    actual_payment_date: "2021-07-06 00:00:00+02:00",
+    actual_settlement_date: "2021-11-16 00:00:00+01:00",
+    date: "2021-07-06 00:00:00+02:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 615693.83,
+    actual_payment_date: "2022-02-15 00:00:00+01:00",
+    actual_settlement_date: null,
+    date: "2022-02-15 00:00:00+01:00",
+    status: "paid",
+  },
+  {
+    invoiced_amount_in_limit_currency: 836406.14,
+    actual_payment_date: "2021-12-03 00:00:00+01:00",
+    actual_settlement_date: "2022-04-01 00:00:00+02:00",
+    date: "2021-12-03 00:00:00+01:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 1194561.04,
+    actual_payment_date: "2021-12-09 00:00:00+01:00",
+    actual_settlement_date: null,
+    date: "2021-12-09 00:00:00+01:00",
+    status: "paid",
+  },
+  {
+    invoiced_amount_in_limit_currency: 333635.96,
+    actual_payment_date: "2020-09-24 00:00:00+02:00",
+    actual_settlement_date: "2020-12-21 14:23:59+01:00",
+    date: "2020-09-24 00:00:00+02:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 151156.35,
+    actual_payment_date: "2020-11-02 00:00:00+01:00",
+    actual_settlement_date: "2020-12-21 14:23:58+01:00",
+    date: "2020-11-02 00:00:00+01:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 794851.66,
+    actual_payment_date: "2020-12-28 00:00:00+01:00",
+    actual_settlement_date: "2021-04-27 14:12:32+02:00",
+    date: "2020-12-28 00:00:00+01:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 527775.73,
+    actual_payment_date: "2021-04-01 00:00:00+02:00",
+    actual_settlement_date: "2021-08-03 00:00:00+02:00",
+    date: "2021-04-01 00:00:00+02:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 1961859.52,
+    actual_payment_date: "2021-09-17 00:00:00+02:00",
+    actual_settlement_date: "2022-03-17 00:00:00+01:00",
+    date: "2021-09-17 00:00:00+02:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 512076.14,
+    actual_payment_date: "2021-05-07 00:00:00+02:00",
+    actual_settlement_date: "2021-09-07 00:00:00+02:00",
+    date: "2021-05-07 00:00:00+02:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 374118.06,
+    actual_payment_date: "2020-06-23 00:00:00+02:00",
+    actual_settlement_date: "2020-10-27 00:00:00+01:00",
+    date: "2020-06-23 00:00:00+02:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 548403.81,
+    actual_payment_date: "2021-09-13 00:00:00+02:00",
+    actual_settlement_date: "2022-01-14 00:00:00+01:00",
+    date: "2021-09-13 00:00:00+02:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 224184.3,
+    actual_payment_date: "2020-09-30 00:00:00+02:00",
+    actual_settlement_date: "2020-12-21 14:23:59+01:00",
+    date: "2020-09-30 00:00:00+02:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 249553.54,
+    actual_payment_date: "2020-10-28 00:00:00+01:00",
+    actual_settlement_date: "2020-12-21 14:23:59+01:00",
+    date: "2020-10-28 00:00:00+01:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 271340.82,
+    actual_payment_date: "2020-11-20 00:00:00+01:00",
+    actual_settlement_date: "2021-03-19 14:22:53+01:00",
+    date: "2020-11-20 00:00:00+01:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 742782.11,
+    actual_payment_date: "2021-08-18 00:00:00+02:00",
+    actual_settlement_date: "2021-12-21 00:00:00+01:00",
+    date: "2021-08-18 00:00:00+02:00",
+    status: "settled",
+  },
+  {
+    invoiced_amount_in_limit_currency: 760046.31,
+    actual_payment_date: "2022-01-17 00:00:00+01:00",
+    actual_settlement_date: null,
+    date: "2022-01-17 00:00:00+01:00",
+    status: "paid",
+  },
+];
+
+// settled to -> payment, payback sorted by date
+// paid to -> payment.
+const getPaymentRow = (dataEntity) => ({
+  type: "payment",
+  creationDate: dataEntity.date,
+  date: dataEntity.actual_payment_date,
+  amount: dataEntity.invoiced_amount_in_limit_currency,
+});
+
+const getPaybackRow = (dataEntity) => ({
+  type: "payback",
+  creationDate: dataEntity.date,
+  date: dataEntity.actual_settlement_date,
+  amount: dataEntity.invoiced_amount_in_limit_currency,
+});
+
+const getTableData = (data) => {
+  return data
+    .sort((first, second) => second.date > first.date)
+    .reduce((prev, next) => {
+      if (next.status === "paid") {
+        return [...prev, getPaymentRow(next)];
+      } else if (next.status === "settled") {
+        const settledRows = [getPaymentRow(next), getPaybackRow(next)].sort(
+          (first, second) => second.date < first.date
+        );
+        return [...prev, ...settledRows];
+      }
+      return prev;
+    }, []);
+};
+
+const getTableHeader = () => {
+  let thead = document.createElement("thead");
+  let headingsRow = document.createElement("tr");
+  let typeHeading = document.createElement("th");
+  typeHeading.innerText = "Type";
+  let dateHeading = document.createElement("th");
+  dateHeading.innerText = "Date";
+  let timeHeading = document.createElement("th");
+  timeHeading.innerText = "Time";
+  let amountHeading = document.createElement("th");
+  amountHeading.innerText = "Amount";
+
+  headingsRow.appendChild(typeHeading);
+  headingsRow.appendChild(dateHeading);
+  headingsRow.appendChild(timeHeading);
+  headingsRow.appendChild(amountHeading);
+  thead.appendChild(headingsRow);
+  return thead;
+};
+
+const getTableBody = () => {
+  let tbody = document.createElement("tbody");
+  const tableData = getTableData(data);
+  tableData.forEach((dataRow) => {
+    const tableRow = document.createElement("tr");
+    const typeCell = document.createElement("td");
+    typeCell.innerText = dataRow.type;
+    const dateCell = document.createElement("td");
+    dateCell.innerText = new Date(dataRow.date).toLocaleDateString();
+    const timeCell = document.createElement("td");
+    timeCell.innerText = new Date(dataRow.date).toLocaleTimeString();
+    const amountCell = document.createElement("td");
+    amountCell.innerText = dataRow.amount;
+
+    tableRow.appendChild(typeCell);
+    tableRow.appendChild(dateCell);
+    tableRow.appendChild(timeCell);
+    tableRow.appendChild(amountCell);
+
+    if (dataRow.type === "payback") {
+      tableRow.classList.add("payback_row");
+    } else {
+      tableRow.classList.add("payment_row");
+    }
+
+    tbody.appendChild(tableRow);
+  });
+  return tbody;
+};
+
+const drawTable = () => {
+  let table = document.createElement("table");
+  table.appendChild(getTableHeader());
+  table.appendChild(getTableBody());
+  document.getElementById("tableContainer").appendChild(table);
+};
+
+drawTable();
